e37c2975df680d0496601a95e7dbcc488d838c1c
schatzjason <jason.schatz@udacity.com>
2015-03-18 18:31:45 -0700

    add PlainMasterDetail

---
+++ PlainMasterDetail/PlainMasterDetail/Event.swift
@@ -0,0 +1,34 @@
+//
+//  Event.swift
+//  PlainMasterDetail
+//
+//  Created by Jason on 3/18/15.
+//  Copyright (c) 2015 Udacity. All rights reserved.
+//
+
+
+/**
+* There are 6 changes to be made. They are listed below, and called out in comments in the
+* code. Notice that the only attribute in this class is "timeStamp", and it gets the current
+* timeStamp by default.
+*
+* 1. import Core Data
+* 2. include the strange statement @objc(Event). This makes Person visible to Core Data code
+* 3. Make Event a subclass of NSManagedObject
+* 4. Add @NSManaged in front of the timeStamp properties/attributes
+* 5. Include the standard Core Data init:
+* 
+*     init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext?)
+*
+* 6. Write an init method that only needs a context. 
+*
+*    init(context: NSManagedObjectContext)
+*/
+
+import Foundation
+
+class Event {
+
+    var timeStamp = NSDate()
+    
+}
