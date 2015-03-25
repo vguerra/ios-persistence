c63b1ed24ceb81dd05713dd0bed9f416dbf5358d
schatzjason <jason.schatz@udacity.com>
2015-03-25 13:33:51 -0700

    Complete the CoreDataMasterDetail solution

---
+++ CoreDataMasterDetail/CoreDataMasterDetail/MasterViewController.swift
@@ -0,0 +1,108 @@
+//
+//  MasterViewController.swift
+//  throwawayMD
+//
+//  Created by Jason on 3/18/15.
+//  Copyright (c) 2015 Udacity. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class MasterViewController: UITableViewController {
+    
+    var objects: [Event]!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.navigationItem.leftBarButtonItem = self.editButtonItem()
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
+        self.navigationItem.rightBarButtonItem = addButton
+        
+        objects = fetchAllEvents()
+    }
+    
+    // Step 5 in the instructions: Add the convenience property
+    
+    lazy var sharedContext: NSManagedObjectContext = {
+       return CoreDataStackManager.sharedInstance().managedObjectContext!
+    }()
+    
+    func fetchAllEvents() -> [Event] {
+        var error: NSError?
+        
+        // Create the Fetch Request
+        let fetchRequest = NSFetchRequest(entityName: "Event")
+        
+        // Execute the Fetch Request
+        let results = sharedContext.executeFetchRequest(fetchRequest, error: &error)
+        
+        // Check for Errors
+        if let error = error? {
+            println("Error in fectchAllActors(): \(error)")
+        }
+        
+        // Return the results, cast to an array of Person objects
+        return results as [Event]
+    }
+    
+    func insertNewObject(sender: AnyObject) {
+        objects.insert(Event(context: sharedContext), atIndex: 0)
+        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+        
+        CoreDataStackManager.sharedInstance().saveContext()
+    }
+    
+    // MARK: - Segues
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = self.tableView.indexPathForSelectedRow() {
+                let object = objects[indexPath.row] as Event
+                (segue.destinationViewController as DetailViewController).detailItem = object
+            }
+        }
+    }
+    
+    // MARK: - Table View
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objects.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        
+        let object = objects[indexPath.row] as Event
+        cell.textLabel!.text = object.timeStamp.description
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            objects.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
+    }
+    
+    
+}
+
