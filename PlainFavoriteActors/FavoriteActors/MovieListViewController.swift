d2b392d3362f2300318958b31dd093ec8f06b0be
schatzjason <jason.schatz@udacity.com>
2015-03-31 11:31:11 -0700

    use the inverse property to relate movie to actor in the broken PlainFavoriteActors

--- PlainFavoriteActors/FavoriteActors/MovieListViewController.swift
+++ PlainFavoriteActors/FavoriteActors/MovieListViewController.swift
@@ -44,7 +44,7 @@ class MovieListViewController : UITableViewController {
                             
                             // We associate this movie with it's actor by appending it to the array
                             // In core data we use the relationship. We set the movie's actor property
-                            self.actor.movies.append(movie)
+                            movie.actor = self.actor
                             
                             return movie
                         }
