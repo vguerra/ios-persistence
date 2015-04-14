6113b6fcb964b97411024813c27472edcb0e5ecc
schatzjason <jason.schatz@udacity.com>
2015-04-14 14:57:02 -0700

    changes for Swift 1.2

--- PlainFavoriteActors/FavoriteActors/TheMovieDB/Person.swift
+++ PlainFavoriteActors/FavoriteActors/TheMovieDB/Person.swift
@@ -45,8 +45,8 @@ class Person : NSManagedObject {
         
         let entity =  NSEntityDescription.entityForName("Person", inManagedObjectContext: context)!
         super.init(entity: entity,insertIntoManagedObjectContext: context)
-        name = dictionary[Keys.Name] as String
-        id = dictionary[Keys.ID] as Int
+        name = dictionary[Keys.Name] as! String
+        id = dictionary[Keys.ID] as! Int
         imagePath = dictionary[Keys.ProfilePath] as? String
     }
     
